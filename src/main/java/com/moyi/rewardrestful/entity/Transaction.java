6b8ffb57781dd46021947a2dec6e7305ff35e435
Zhipeng yin <zhip_y@yeah.net>
2023-02-09 23:55:07 -0500

    fix bug

--- src/main/java/com/moyi/rewardrestful/entity/Transaction.java
+++ src/main/java/com/moyi/rewardrestful/entity/Transaction.java
@@ -2,8 +2,6 @@ package com.moyi.rewardrestful.entity;
 
 
 
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
